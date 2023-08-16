426e53e99477e7046f6aa5a2561b0d41da51e86f
Vincenzo Sicurella <sicurella12@gmail.com>
2023-08-15 22:10:47 -0400

    add mapped key helper class

--- Source/lumatone_editor_library/data/lumatone_key.h
+++ Source/lumatone_editor_library/data/lumatone_key.h
@@ -17,26 +17,46 @@
 class LumatoneKey
 {
 public:
-	LumatoneKey(LumatoneKeyType newKeyType = LumatoneKeyType::disabled) { noteNumber = 0; channelNumber = 1; colour = juce::Colour(); keyType = newKeyType; ccFaderDefault = true; };
-	LumatoneKey(LumatoneKeyType newKeyType, int newChannelNumber, int newNoteNumber, juce::Colour newColour, bool invertCCFader = false)
-	{
-		keyType = newKeyType; channelNumber = newChannelNumber; noteNumber = newNoteNumber; colour = newColour; ccFaderDefault = invertCCFader;
-	}
-	bool isEmpty() const { return channelNumber == 0; }
+    LumatoneKey(LumatoneKeyType newKeyType = LumatoneKeyType::disabled) { noteNumber = 0; channelNumber = 1; colour = juce::Colour(); keyType = newKeyType; ccFaderDefault = true; };
+    LumatoneKey(LumatoneKeyType newKeyType, int newChannelNumber, int newNoteNumber, juce::Colour newColour, bool invertCCFader = false)
+    {
+        keyType = newKeyType; channelNumber = newChannelNumber; noteNumber = newNoteNumber; colour = newColour; ccFaderDefault = invertCCFader;
+    }
+    bool isEmpty() const { return channelNumber == 0; }
 
-	bool operator!=(const LumatoneKey& second) const { return noteNumber != second.noteNumber || channelNumber != second.channelNumber || colour != second.colour || keyType != second.keyType || ccFaderDefault != second.ccFaderDefault; }
+    bool operator!=(const LumatoneKey& second) const { return noteNumber != second.noteNumber || channelNumber != second.channelNumber || colour != second.colour || keyType != second.keyType || ccFaderDefault != second.ccFaderDefault; }
 
-	// Chainable setters
-	LumatoneKey withKeyType(LumatoneKeyType type) const { return LumatoneKey(type, channelNumber, noteNumber, colour, ccFaderDefault); }
-	LumatoneKey withColour(juce::Colour newColour) const { return LumatoneKey(keyType, channelNumber, noteNumber, newColour, ccFaderDefault); }
-	LumatoneKey withNoteOrCC(int noteOrCC) const { return LumatoneKey(keyType, channelNumber, noteOrCC, colour, ccFaderDefault); }
-	LumatoneKey withChannelNumber(int channel) const { return LumatoneKey(keyType, channel, noteNumber, colour, ccFaderDefault); }
-	LumatoneKey withInvertCCFader(bool invertCCFader) const { return LumatoneKey(keyType, channelNumber, noteNumber, colour, invertCCFader); }
+    // Chainable setters
+    LumatoneKey withKeyType(LumatoneKeyType type) const { return LumatoneKey(type, channelNumber, noteNumber, colour, ccFaderDefault); }
+    LumatoneKey withColour(juce::Colour newColour) const { return LumatoneKey(keyType, channelNumber, noteNumber, newColour, ccFaderDefault); }
+    LumatoneKey withNoteOrCC(int noteOrCC) const { return LumatoneKey(keyType, channelNumber, noteOrCC, colour, ccFaderDefault); }
+    LumatoneKey withChannelNumber(int channel) const { return LumatoneKey(keyType, channel, noteNumber, colour, ccFaderDefault); }
+    LumatoneKey withInvertCCFader(bool invertCCFader) const { return LumatoneKey(keyType, channelNumber, noteNumber, colour, invertCCFader); }
 
 public:
-	int				noteNumber;
-	int			    channelNumber;
-	juce::Colour    colour;
-	bool            ccFaderDefault;
-	LumatoneKeyType	keyType;
+    int                noteNumber;
+    int                channelNumber;
+    juce::Colour       colour;
+    bool               ccFaderDefault;
+    LumatoneKeyType    keyType;
+};
+
+class MappedLumatoneKey : public LumatoneKey
+{
+public:
+    MappedLumatoneKey(LumatoneKey key, int boardIndexIn, int keyIndexIn)
+        : LumatoneKey(key)
+		, boardIndex(boardIndexIn)
+        , keyIndex(keyIndexIn)
+        { }
+
+	MappedLumatoneKey(LumatoneKey key, LumatoneKeyCoord coord)
+		: MappedLumatoneKey(key, coord.boardIndex, coord.keyIndex) {}
+
+    LumatoneKeyCoord getKeyCoord() const { return LumatoneKeyCoord(boardIndex, keyIndex); }
+
+public:
+
+    int boardIndex = -1;
+    int keyIndex = -1;
 };
