9d49bfb5118d1e4604fc82ecb005c249b38523e2
Vincenzo Sicurella <sicurella12@gmail.com>
2023-09-04 19:18:07 -0400

    draft of LumatoneContext class

---
+++ Source/shared/lumatone_editor_library/data/lumatone_context.cpp
@@ -0,0 +1,102 @@
+#include "lumatone_context.h"
+
+LumatoneContext::LumatoneContext(const LumatoneLayout &layout)
+    : LumatoneLayout(layout)
+{
+
+}
+
+LumatoneContext::LumatoneContext(const LumatoneContext &context)
+    : LumatoneContext(context, (const LumatoneLayout&)context)
+{
+
+}
+
+LumatoneContext::LumatoneContext(const LumatoneContext &context, const LumatoneLayout &layout)
+    : LumatoneLayout(layout)
+    , midiChannelsMapped(context.midiChannelsMapped)
+    , mappedMidiChannels(context.mappedMidiChannels)
+    , midiNotesMapped(context.midiNotesMapped)
+    , mappedMidiNotes(context.mappedMidiNotes)
+{
+
+}
+
+LumatoneContext::~LumatoneContext()
+{
+}
+
+bool LumatoneContext::setMappedMidiChannels(juce::Array<int> midiChannelMap)
+{
+    const int expectedSize = getNumBoards() * getOctaveBoardSize(); // TODO cache this
+    
+    if (midiChannelMap.size() != expectedSize)
+        return false;
+
+    juce::ScopedLock l(mappedMidiChannels.getLock());
+    for (int i = 0; i < expectedSize; i++)
+    {
+        mappedMidiChannels.set(i, midiChannelMap[i]);
+    }
+
+    return true;
+}
+
+void LumatoneContext::clearMappedMidiChannels()
+{
+    juce::ScopedLock l(mappedMidiChannels.getLock());
+
+    mappedMidiChannels.clear();
+    midiChannelsMapped = false;
+}
+
+bool LumatoneContext::setMappedMidiNotes(juce::Array<int> midiNoteMap)
+{
+    const int expectedSize = getNumBoards() * getOctaveBoardSize(); // TODO cache this
+    
+    if (midiNoteMap.size() != expectedSize)
+        return false;
+
+    juce::ScopedLock l(mappedMidiNotes.getLock());
+    for (int i = 0; i < expectedSize; i++)
+    {
+        mappedMidiNotes.set(i, midiNoteMap[i]);
+    }
+
+    return true;
+}
+
+void LumatoneContext::clearMappedMidiNotes()
+{
+    juce::ScopedLock l(mappedMidiNotes.getLock());
+    
+    mappedMidiNotes.clear();
+    midiNotesMapped = false;
+}
+
+LumatoneKeyContext LumatoneContext::getKeyContext(int boardIndex, int keyIndex) const
+{
+    auto coord = LumatoneKeyCoord(boardIndex, keyIndex);
+    if (!isKeyCoordValid(coord))
+        return LumatoneKeyContext();
+
+    auto keyNum = keyCoordToKeyNum(coord);
+    MappedLumatoneKey mappedKey = MappedLumatoneKey(*readKey(boardIndex, keyIndex), coord);
+    LumatoneKeyContext keyContext = LumatoneKeyContext(mappedKey);
+
+    // TODO make a cache of this
+
+    if (midiChannelsMapped)
+        keyContext.channelNumber = mappedMidiChannels[keyNum];
+
+    if (midiNotesMapped)
+        keyContext.noteNumber = mappedMidiNotes[keyNum];
+
+    return keyContext;
+}
+
+LumatoneKeyContext LumatoneContext::getKeyContext(int keyNum) const
+{
+    auto coord = keyNumToKeyCoord(keyNum);
+    return getKeyContext(coord.boardIndex, coord.keyIndex);
+}
---
+++ Source/shared/lumatone_editor_library/data/lumatone_context.h
@@ -0,0 +1,42 @@
+// Supply pitch/note, tuning, or remapped configs for a LumatoneLayout
+
+#pragma once
+
+#include "./lumatone_layout.h"
+
+struct LumatoneKeyContext : public MappedLumatoneKey
+{
+    LumatoneKeyContext() : MappedLumatoneKey() {}
+
+    LumatoneKeyContext(const MappedLumatoneKey& key) : MappedLumatoneKey(key) {}
+
+    // TODO add more properties
+};
+
+class LumatoneContext : public LumatoneLayout
+{
+public:
+
+    LumatoneContext(const LumatoneLayout& layout);
+    LumatoneContext(const LumatoneContext& context);
+    LumatoneContext(const LumatoneContext& context, const LumatoneLayout& layout);
+    
+    ~LumatoneContext();
+
+    bool setMappedMidiChannels(juce::Array<int> midiChannelMap);
+    void clearMappedMidiChannels();
+
+    bool setMappedMidiNotes(juce::Array<int> midiNoteMap);
+    void clearMappedMidiNotes();
+
+    LumatoneKeyContext getKeyContext(int boardIndex, int keyIndex) const;
+    LumatoneKeyContext getKeyContext(int keyNum) const;
+
+private:
+
+    bool midiChannelsMapped = false;
+    juce::Array<int, juce::CriticalSection> mappedMidiChannels;
+
+    bool midiNotesMapped = false;
+    juce::Array<int, juce::CriticalSection> mappedMidiNotes;
+};
